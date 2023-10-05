199a966257ca8de987e6b20c624dc4d07ccdbf6e
PJarana <pablojosejarana21@gmail.com>
2023-10-05 19:27:30 +0200

    adapter for output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  server:{
-    watch:{
-      usePolling:true
+  server: {
+    watch: {
+      usePolling: true
     },
     host: true
-  }
+  },
+  adapter: vercel()
 });
