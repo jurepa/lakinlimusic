6d32ac33a63b671175fa4bb78675edba9b4b3c2a
PJarana <pablojosejarana21@gmail.com>
2023-10-05 19:07:37 +0200

    first commit

---
+++ spotify/src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greeting = ""
+    if(currentHour < 12){
+        greeting = "Buenos días"
+    }else if(currentHour < 21){
+        greeting = "Escuchado recientemente"
+    }else{
+        greeting = "Buenas noches"
+    }
+
+</script>
+
+<h1 class="text-4xl font-bold">{greeting}</h1>
